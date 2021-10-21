ac9501db2c92ced20720ea5f223b51b732ddaae5
saidinesh898 <saidinesh898@gmail.com>
2021-10-21 22:05:42 +0530

    commit after staging all

--- app.js
+++ app.js
@@ -1,18 +1,84 @@
-const geocode = require('./utils/geocode')
-const forcast = require('./utils/forcast')
+const geocode = require('./src/geocode')
+const forcast = require('./src/forcast')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
 
-if (process.argv[2]) {
-    geocode(process.argv[2], (error, data) => {
+
+const viewsPath = path.join(__dirname, '../weather-app/templates/views')
+const partialsPath = path.join(__dirname, '../weather-app/templates/partials')
+
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, '../weather-app/public')))
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        temp: 30,
+        location: "Mumbai is Gey",
+        svg: "cloudy.svg"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.send("<h1>Help</h1>")
+})
+app.get('/weather', (req, res) => {
+    if (!req.query.location) {
+        return res.send({
+            error: "You must provide an Address"
+        })
+    }
+    geocode(req.query.location, (error, data) => {
         if (error) {
-            return console.log(error)
+            return res.send({
+                error
+            })
         } else {
             forcast(data.lat + ',' + data.long, (error, ForcastData) => {
-                console.log(data.location)
-                console.log(ForcastData)
+                if (error) {
+                    return res.send({
+                        error
+                    })
+                }
+                res.send({
+                    data,
+                    ForcastData,
+                })
             })
         }
 
     })
-} else {
-    console.log("Please provide a location")
-}
+
+})
+
+
+
+
+
+app.listen(80, () => {
+    console.log('server started on port ', 80)
+})
+
+
+
+
+
+// if (process.argv[2]) {
+//     geocode(process.argv[2], (error, data) => {
+//         if (error) {
+//             return console.log(error)
+//         } else {
+//             forcast(data.lat + ',' + data.long, (error, ForcastData) => {
+//                 console.log(data.location)
+//                 console.log(ForcastData)
+//             })
+//         }
+
+//     })
+// } else {
+//     console.log("Please provide a location")
+// }
